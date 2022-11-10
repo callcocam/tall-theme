c927f2dda35c6564f0a4ed143c9d88149b5b02cd
Claudio Campos <callcocam@gmail.com>
2022-11-10 14:23:53 -0300

    delete modal

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,4 +1,4 @@
 <button
-    {{ $attributes->merge(['type' => 'button', 'class' => 'btn bg-primary font-medium text-white hover:bg-primary-focus focus:bg-primary-focus active:bg-primary-focus/90 dark:bg-accent dark:hover:bg-accent-focus dark:focus:bg-accent-focus dark:active:bg-accent/90']) }}>
+    {{ $attributes->merge(['type' => 'button']) }}>
     {{ $slot }}
 </button>
