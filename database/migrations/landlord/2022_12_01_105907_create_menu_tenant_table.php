5a1d3072456b858c2fef696df3cc29c899c71f34
Claudio Campos <callcocam@gmail.com>
2022-12-02 13:26:49 -0300

    automatico

---
+++ database/migrations/landlord/2022_12_01_105907_create_menu_tenant_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('menu_tenant', function (Blueprint $table) {
+            $table->foreignUuid('menu_id')->constrained('menus')->cascadeOnDelete();
+            $table->foreignUuid('tenant_id')->constrained('tenants')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('menu_tenant');
+    }
+};
